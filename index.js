80af96611b1d5e5bc5d0719cbab2e56855534528
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2022-01-02 17:34:37 +0700

    add social icons to public assets

--- index.js
+++ index.js
@@ -4,31 +4,37 @@ customScript = {
         'facebook' : {
             "name" : "Facebook",
             "url" : "https://www.facebook.com/vikingcurvasud",
-            "src" : "https://www.iconsdb.com/icons/preview/gray/facebook-6-xxl.png",
+            "src" : "public/img/facebook-6-xxl.png",
             'class' : "w-6 rounded-full mr-3"
         },
         'twitter' : {
             "name" : "Twitter",
             "url" : "https://www.twitter.com/chaobianconerri",
-            "src" : "https://www.iconsdb.com/icons/preview/gray/twitter-3-xxl.png",
+            "src" : "public/img/twitter-3-xxl.png",
             'class' : "w-6 rounded-full mr-3"
         },
         'instagram' : {
             "name" : "Instagram",
             "url" : "https://www.instagram.com/kholis.cahyoko",
-            "src" : "https://www.iconsdb.com/icons/preview/gray/instagram-6-xxl.png",
+            "src" : "public/img/instagram-6-xxl.png",
             'class' : "w-6 rounded-full mr-3"
         },
         'linkedin' : {
             "name" : "LinkedIn",
             "url" : "https://www.linkedin.com/in/kholis-cahyoko-2455541a0",
-            "src" : "https://www.iconsdb.com/icons/preview/gray/linkedin-6-xxl.png",
+            "src" : "public/img/linkedin-6-xxl.png",
+            'class' : "w-6 rounded-full mr-3"
+        },
+        'wordpress' : {
+            "name" : "Wordpress",
+            "url" : "https://kholiscahyoko.my.id",
+            "src" : "public/img/wordpress-6-xxl.png",
             'class' : "w-6 rounded-full mr-3"
         },
         'github' : {
             "name" : "Github",
             "url" : "https://github.com/kholiscahyoko",
-            "src" : "https://www.iconsdb.com/icons/preview/gray/github-8-xxl.png",
+            "src" : "public/img/github-8-xxl.png",
             'class' : "w-6 rounded-full"
         },
     },
@@ -36,13 +42,7 @@ customScript = {
     init : function(){
         var self = this;
         for (const socialKey in self.socials) {
-
-            console.log(self.socials[socialKey]);
             document.getElementById("social-links").appendChild(self.createSocialIcon(self.socials[socialKey]));
-            // if (Object.hasOwnProperty.call(object, key)) {
-            //     const element = object[key];
-                
-            // }
         }
     },
 
